1ced9590ab40619772fc395e80a8f16e25edb80f
Oscar Cederberg <os7138ce-s@student.lu.se>
2022-01-03 20:36:13 +0100

    Add flex

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,16 +2,18 @@ import * as React from 'react'
 
 const Layout = ({ pageTitle, subTitle, lyricsText, children }) => {
   return (
-    <div>
+    <>
       <title>{pageTitle}</title>
-      <main className="text-center">
-        <p className="text-gray-200 font-lyrics"> {lyricsText} </p>
-        <h1 className="text-7xl font-title lowercase font-bold mt-1"> {pageTitle} </h1>
-        <h1 className="text-3xl font-title uppercase font-extralight mb-1"> {subTitle} </h1>
-        {children}
-        <p className="text-gray-200 font-lyrics"> {lyricsText.reverse()} </p>
+      <main className="flex flex-col justify-between items-center text-center h-screen">
+        <p className="text-l text-gray-200 font-lyrics my-2"> {lyricsText} </p>
+        <div className=''>
+          <h1 className="text-8xl font-title lowercase font-bold"> {pageTitle} </h1>
+          <h1 className="text-5xl font-title uppercase font-extralight -mt-2"> {subTitle} </h1>
+          <div className='mb-1'> {children} </div>
+        </div>
+        <p className="text-xl text-gray-200 font-lyrics my-2"> {[...lyricsText].reverse()} </p>
       </main>
-    </div>
+    </>
   )
 }
 
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,25 @@
 import * as React from "react"
+import Layout from "../components/layout"
 
-// markup
+const lyrics=[
+  'between boredom, boredom and horror - between the kingdom of the living and the dead',
+  <br />,
+  'mellan tristess, tristess och skräck - mellan riket av de levande och de döda',
+  <br />,
+  'entre l\'ennui, l\'ennui et l\'horreur - entre le royaume des vivants et des morts',
+  <br />,
+  'entre el aburrimiento, el aburrimiento y el horror - entre el reino de los vivos y los muertos',
+  <br />,
+  'zwischen langeweile, langeweile und entsetzen - zwischen dem reich der lebenden und der toten',
+  <br />,
+  '在无聊，无聊和恐怖之间-在生与死的领域之间',
+  <br />,
+  '退屈、退屈と恐怖の間 - 生きている王国と死んだ王国の間'
+];
+
 const AboutPage = () => {
   return (
-    <Layout pageTitle={"About"} subTitle={"under construction"}>
+    <Layout pageTitle={"About"} subTitle={"under construction"} lyricsText={lyrics}>
       <p>My personal website.</p>
     </Layout>
   )
