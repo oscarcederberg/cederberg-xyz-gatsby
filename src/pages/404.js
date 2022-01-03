296568606bcff1d95accf62b5b95793163b16777
Oscar Cederberg <os7138ce-s@student.lu.se>
2022-01-03 20:44:48 +0100

    Fix 404 page, remove icon

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,26 @@
 import * as React from "react"
+import Layout from "../components/layout"
+
+const lyrics=[
+  'a generation lost in space, with no time left to start again',
+  <br />,
+  'en generation förlorad i rymden, utan tid kvar att börja om',
+  <br />,
+  'une génération perdue dans l\'espace, sans plus de temps pour recommencer',
+  <br />,
+  'una generación perdida en el espacio, sin tiempo para empezar de nuevo',
+  <br />,
+  'eine im weltraum verlorene generation, die keine zeit mehr hat, neu anzufangen',
+  <br />,
+  '一代人迷失在太空中，没有时间重新开始',
+  <br />,
+  '宇宙で失われた世代、再開する時間がない'
+];
 
 const NotFoundPage = () => {
   return (
-    <main>
-      <title>404</title>
-      <h1>404</h1>
-    </main>
+    <Layout pageTitle={"error 404"} subTitle={"not found"} lyricsText={lyrics}>
+    </Layout>
   )
 }
 
