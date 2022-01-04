43e42b3c9f5a5564bd255bb5247e4a6eb72363d1
oscarcederberg <os7138ce-s@student.lu.se>
2022-01-04 15:08:41 +0100

    Update lyrics

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,13 +8,7 @@ const lyrics=[
   '\n',
   'entre l\'ennui, l\'ennui et l\'horreur - entre le royaume des vivants et des morts',
   '\n',
-  'entre el aburrimiento, el aburrimiento y el horror - entre el reino de los vivos y los muertos',
-  '\n',
-  'zwischen langeweile, langeweile und entsetzen - zwischen dem reich der lebenden und der toten',
-  '\n',
-  '在无聊，无聊和恐怖之间-在生与死的领域之间',
-  '\n',
-  '退屈、退屈と恐怖の間 - 生きている王国と死んだ王国の間'
+  'entre el aburrimiento, el aburrimiento y el horror - entre en el reino de los vivos y los muertos',
 ];
 
 const IndexPage = () => {
