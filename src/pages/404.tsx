165fb90b0009b7acd447becba308f1cf8efc279f
oscarcederberg <os7138ce-s@student.lu.se>
2022-01-04 15:08:51 +0100

    Update lyrics

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -6,15 +6,9 @@ const lyrics=[
   '\n',
   'en generation förlorad i rymden, utan tid kvar att börja om',
   '\n',
-  'une génération perdue dans l\'espace, sans plus de temps pour recommencer',
+  'une génération perdue dans l\'espace, sans le temps pour recommencer',
   '\n',
   'una generación perdida en el espacio, sin tiempo para empezar de nuevo',
-  '\n',
-  'eine im weltraum verlorene generation, die keine zeit mehr hat, neu anzufangen',
-  '\n',
-  '一代人迷失在太空中，没有时间重新开始',
-  '\n',
-  '宇宙で失われた世代、再開する時間がない'
 ];
 
 const NotFoundPage = () => {
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -9,12 +9,6 @@ const lyrics=[
   'entre l\'ennui, l\'ennui et l\'horreur - entre le royaume des vivants et des morts',
   '\n',
   'entre el aburrimiento, el aburrimiento y el horror - entre el reino de los vivos y los muertos',
-  '\n',
-  'zwischen langeweile, langeweile und entsetzen - zwischen dem reich der lebenden und der toten',
-  '\n',
-  '在无聊，无聊和恐怖之间-在生与死的领域之间',
-  '\n',
-  '退屈、退屈と恐怖の間 - 生きている王国と死んだ王国の間'
 ];
 
 const AboutPage = () => {
