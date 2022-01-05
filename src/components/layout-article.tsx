b563e6913edfa21390ec06d4e1707e032f70b29f
oscarcederberg <os7138ce-s@student.lu.se>
2022-01-05 13:58:01 +0100

    Fix z-index

--- src/components/layout-article.tsx
+++ src/components/layout-article.tsx
@@ -13,24 +13,25 @@ export default function LayoutArticle(props: LayoutProps) {
     <>
       <title>{props.pageTitle}</title>
       <main className="relative text-center h-screen v-screen">
-        <p className="absolute inset-x-0 top-0
+        <p className="-z-10 absolute inset-x-0 top-0
         text-xs sm:text-sm lg:text-lg
         text-gray-300 font-lyrics leading-tight whitespace-pre-line my-2"> {props.lyricsText} </p>
 
-        <div className='text-left mx-16'>
+        <div className='z-10 text-left mx-16'>
           <div className='flex flex-row items-center gap-x-4'>
             <h1 className="text-4xl sm:text-6xl lg:text-8xl
             font-title lowercase font-bold"> {props.pageTitle} </h1>
-            <h1 className="text-xl sm:text-3xl lg:text-5xl 
-            font-title uppercase font-extralight"> {props.subTitle} </h1>
+            <h1 className="text-4xl sm:text-6xl lg:text-8xl 
+            font-title lowercase font-extralight"> {props.subTitle} </h1>
           </div>
+
           <div> 
             <NavBar />
             {props.children} 
           </div>
         </div>
 
-        <p className="absolute inset-x-0 bottom-0
+        <p className="-z-10 absolute inset-x-0 bottom-0
         text-xs sm:text-sm lg:text-lg
         text-gray-300 font-lyrics leading-tight whitespace-pre-line my-2"> {[...props.lyricsText].reverse()} </p>
       </main>
--- src/components/layout-center.tsx
+++ src/components/layout-center.tsx
@@ -13,11 +13,11 @@ export default function LayoutCenter(props: LayoutProps) {
     <>
       <title>{props.pageTitle}</title>
       <main className="relative text-center h-screen">
-        <p className="absolute inset-x-0 top-0
+        <p className="-z-10 absolute inset-x-0 top-0
         text-xs sm:text-sm lg:text-lg
         text-gray-300 font-lyrics leading-tight whitespace-pre-line my-2"> {props.lyricsText} </p>
 
-        <div className='absolute inset-24 z-10 
+        <div className='z-10 absolute inset-24 
         flex flex-col justify-evenly items-center'>
           <div className='w-80 sm:w-96 lg:w-2/3'>
             <h1 className="text-4xl sm:text-6xl lg:text-8xl
@@ -31,7 +31,7 @@ export default function LayoutCenter(props: LayoutProps) {
           </div>
         </div>
 
-        <p className="absolute inset-x-0 bottom-0
+        <p className="-z-10 absolute inset-x-0 bottom-0
         text-xs sm:text-sm lg:text-lg
         text-gray-300 font-lyrics leading-tight whitespace-pre-line my-2"> {[...props.lyricsText].reverse()} </p>
       </main>
