194d5ff454570b6808dc99182b9bcfaf6254a05f
Oscar Cederberg <os7138ce-s@student.lu.se>
2022-02-01 23:54:05 +0100

    Add divider

--- src/components/layout-article.tsx
+++ src/components/layout-article.tsx
@@ -27,6 +27,16 @@ export default function LayoutArticle({pageTitle, subTitle, lyricsText, children
             <NavBar style="flex flex-row justify-evenly text-center w-80" />
           </div>
 
+          <p className="-z-10 absolute inset-x-0 -left-20
+          text-gray-300 font-lyrics text-xs sm:text-sm lg:text-lg
+          lowercase tracking-tight whitespace-nowrap">
+            it's good to be king. wait, maybe. i think maybe i'm just like a little bizarre little person who walks back and forth. whatever, you know, but...
+            det är bra att vara kung. vänta, kanske. jag tror kanske jag är som en liten bisarr liten person som går fram och tillbaka. vad som helst, du vet, men...
+            c'est bien d'être roi. attendez, peut-être. je pense que je suis peut-être comme une petite personne bizarre qui va et vient. peu importe, tu sais, mais...
+            es bueno ser rey. espera, tal vez. creo que tal vez soy como una pequeña personita extraña que camina de un lado a otro. lo que sea, ya sabes, pero...
+            왕이 되는 것이 좋습니다. 잠깐, 아마도. 나는 아마도 앞뒤로 걷는 작고 기이 한 작은 사람과 같다고 생각합니다. 뭐, 알겠지만...
+          </p>
+          
           <div className="my-10">
             {children} 
           </div> 
