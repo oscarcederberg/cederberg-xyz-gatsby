0efb4869e00fa7bebc61bad5633b1ce08c1416a1
Oscar Cederberg <os7138ce-s@student.lu.se>
2022-02-02 22:52:18 +0100

    Add scrolling animation for divider

--- src/components/layout-article.tsx
+++ src/components/layout-article.tsx
@@ -14,24 +14,27 @@ export default function LayoutArticle({pageTitle, subTitle, lyricsText, children
     <>
       <title>{pageTitle} | {subTitle}</title>
       
-      <main className="relative text-center h-screen v-screen">
-        <div className='z-10 text-left mx-16 mt-6'>
-          <div className='flex flex-col sm:flex-row items-center'>
+      <main className="relative h-screen text-center v-screen">
+        <div className='z-10 mx-16 mt-6 text-left'>
+          <div className='flex flex-col items-center sm:flex-row'>
             <div className='flex flex-row items-center gap-x-4'>
-              <h1 className="text-xl sm:text-3xl lg:text-5xl
-              font-title lowercase font-bold"> {pageTitle} </h1>
+              <h1 className="text-xl font-bold lowercase sm:text-3xl lg:text-5xl font-title"> {pageTitle} </h1>
 
-              <h1 className="text-xl sm:text-3xl lg:text-5xl 
-              font-title lowercase font-extralight"> {subTitle} </h1>
+              <h1 className="text-xl lowercase sm:text-3xl lg:text-5xl font-title font-extralight"> {subTitle} </h1>
             </div>
             <NavBar style="flex flex-row justify-evenly text-center w-80" />
           </div>
 
-          <p className="-z-10 absolute inset-x-0 -left-20
-          text-gray-300 font-lyrics text-xs sm:text-sm lg:text-lg
-          lowercase tracking-tight whitespace-nowrap">
+          <p className="absolute w-full text-xs tracking-tight text-gray-300 lowercase -z-10 font-lyrics sm:text-sm lg:text-lg whitespace-nowrap animate-scroll_lp">
             it's good to be king. wait, maybe. i think maybe i'm just like a little bizarre little person who walks back and forth. whatever, you know, but...
-            det är bra att vara kung. vänta, kanske. jag tror kanske jag är som en liten bisarr liten person som går fram och tillbaka. vad som helst, du vet, men...
+            det är bra att vara kung. vänta, kanske. jag tror kanske jag är som en liten bisarr liten person som går fram och tillbaka. ändå, du vet, men...
+            c'est bien d'être roi. attendez, peut-être. je pense que je suis peut-être comme une petite personne bizarre qui va et vient. peu importe, tu sais, mais...
+            es bueno ser rey. espera, tal vez. creo que tal vez soy como una pequeña personita extraña que camina de un lado a otro. lo que sea, ya sabes, pero...
+            왕이 되는 것이 좋습니다. 잠깐, 아마도. 나는 아마도 앞뒤로 걷는 작고 기이 한 작은 사람과 같다고 생각합니다. 뭐, 알겠지만...
+          </p>
+          <p className="absolute w-full text-xs tracking-tight text-gray-300 lowercase -z-10 font-lyrics sm:text-sm lg:text-lg whitespace-nowrap animate-scroll_rp">
+            it's good to be king. wait, maybe. i think maybe i'm just like a little bizarre little person who walks back and forth. whatever, you know, but...
+            det är bra att vara kung. vänta, kanske. jag tror kanske jag är som en liten bisarr liten person som går fram och tillbaka. ändå, du vet, men...
             c'est bien d'être roi. attendez, peut-être. je pense que je suis peut-être comme une petite personne bizarre qui va et vient. peu importe, tu sais, mais...
             es bueno ser rey. espera, tal vez. creo que tal vez soy como una pequeña personita extraña que camina de un lado a otro. lo que sea, ya sabes, pero...
             왕이 되는 것이 좋습니다. 잠깐, 아마도. 나는 아마도 앞뒤로 걷는 작고 기이 한 작은 사람과 같다고 생각합니다. 뭐, 알겠지만...
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,22 @@ module.exports = {
       'title': ['Arial','Helvetica','sans-serif'],
       'lyrics': ['"Times New Roman"','Times','serif']
     },
-    extend: {},
+    extend: {
+      keyframes: {
+        scroll_lp: {
+          from: { transform: 'translateX(0%)'},
+          to: { transform: 'translateX(-255%)'}
+        },
+        scroll_rp: {
+          from: { transform: 'translateX(255%)'},
+          to: { transform: 'translateX(0%)'}
+        },
+      },
+      animation: {
+        scroll_lp: 'scroll_lp 24s linear infinite',
+        scroll_rp: 'scroll_rp 24s linear infinite',
+      },
+    },
   },
   plugins: [],
 }
