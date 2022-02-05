8af5e614dc6b4602b6d2f4959ad76e268922dc7a
Oscar Cederberg <os7138ce-s@student.lu.se>
2022-02-05 14:14:50 +0100

    Refactor Divider

--- src/components/layout-article.tsx
+++ src/components/layout-article.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import Divider from './divider'
 import { LyricsBottom } from './lyrics'
 import NavBar from './navbar'
 
@@ -6,10 +7,11 @@ interface LayoutProps {
   pageTitle: string;
   subTitle: string;
   lyricsText: string[];
+  dividerText: string;
   children: any;
 }
 
-export default function LayoutArticle({pageTitle, subTitle, lyricsText, children}: LayoutProps) {
+export default function LayoutArticle({pageTitle, subTitle, lyricsText, dividerText, children}: LayoutProps) {
   return (
     <>
       <title>{pageTitle} | {subTitle}</title>
@@ -26,22 +28,7 @@ export default function LayoutArticle({pageTitle, subTitle, lyricsText, children
           </div>
         </div>
         
-        <div className="relative h-10 overflow-x-hidden text-xs tracking-tight text-gray-300 lowercase dark:text-gray-700 font-lyrics sm:text-sm lg:text-lg whitespace-nowrap">
-          <p className="absolute align-middle animate-scroll_lp">
-            it's good to be king. wait, maybe. i think maybe i'm just like a little bizarre little person who walks back and forth. whatever, you know, but...
-            det är bra att vara kung. vänta, kanske. jag tror kanske jag är som en liten bisarr liten person som går fram och tillbaka. ändå, du vet, men...
-            c'est bien d'être roi. attendez, peut-être. je pense que je suis peut-être comme une petite personne bizarre qui va et vient. peu importe, tu sais, mais...
-            es bueno ser rey. espera, quizás. creo qué parezco ser simplemente un extraño pequeño y pequeña persona que camina para atrás y para adelante. aún, tu sabes, pero
-            왕이 되는 것이 좋습니다. 잠깐, 아마도. 나는 아마도 앞뒤로 걷는 작고 기이 한 작은 사람과 같다고 생각합니다. 뭐, 알겠지만...
-          </p>
-          <p className="absolute align-middle animate-scroll_rp">
-            it's good to be king. wait, maybe. i think maybe i'm just like a little bizarre little person who walks back and forth. whatever, you know, but...
-            det är bra att vara kung. vänta, kanske. jag tror kanske jag är som en liten bisarr liten person som går fram och tillbaka. ändå, du vet, men...
-            c'est bien d'être roi. attendez, peut-être. je pense que je suis peut-être comme une petite personne bizarre qui va et vient. peu importe, tu sais, mais...
-            es bueno ser rey. espera, quizás. creo qué parezco ser simplemente un extraño pequeño y pequeña persona que camina para atrás y para adelante. aún, tu sabes, pero
-            왕이 되는 것이 좋습니다. 잠깐, 아마도. 나는 아마도 앞뒤로 걷는 작고 기이 한 작은 사람과 같다고 생각합니다. 뭐, 알겠지만...
-          </p>
-        </div>
+        <Divider text={dividerText} />
           
         <div className="mx-16 text-left">
           {children} 
