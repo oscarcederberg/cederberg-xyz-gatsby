1ef682bec6cd40ecfb3eace8f220ddf30ec2516b
Oscar Cederberg <os7138ce-s@student.lu.se>
2022-02-05 12:54:11 +0100

    Fix divider

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,16 +11,16 @@ module.exports = {
       keyframes: {
         scroll_lp: {
           from: { transform: 'translateX(0%)'},
-          to: { transform: 'translateX(-255%)'}
+          to: { transform: 'translateX(-100%)'}
         },
         scroll_rp: {
-          from: { transform: 'translateX(255%)'},
+          from: { transform: 'translateX(100%)'},
           to: { transform: 'translateX(0%)'}
         },
       },
       animation: {
-        scroll_lp: 'scroll_lp 48s linear infinite',
-        scroll_rp: 'scroll_rp 48s linear infinite',
+        scroll_lp: 'scroll_lp 16s linear infinite',
+        scroll_rp: 'scroll_rp 16s linear infinite',
       },
     },
   },
